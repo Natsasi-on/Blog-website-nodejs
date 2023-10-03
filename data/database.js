f85e6b39c37353f094f338fdcb44d2de1dc602ac
Natsasion Srithiowat <100752017+Natsasi-on@users.noreply.github.com>
2023-10-03 03:33:07 -0400

    Update database.js

--- data/database.js
+++ data/database.js
@@ -1,17 +1,17 @@
 const mysql = require('mysql2/promise');
 
-// const pool = mysql.createPool({
-//     host: 'buiwarp0roi2pe7kywvn-mysql.services.clever-cloud.com',
-//     database: 'buiwarp0roi2pe7kywvn',
-//     user: 'uaj0ljkkrp0ymah5',
-//     password: 'mSNRRbvOz84R4UbnwF4D'
-// });
-
 const pool = mysql.createPool({
-    host: 'localhost',
-    database: 'blog',
-    user: 'root',
-    port: 3308,
-    password: ''
+    host: 'buiwarp0roi2pe7kywvn-mysql.services.clever-cloud.com',
+    database: 'buiwarp0roi2pe7kywvn',
+    user: 'uaj0ljkkrp0ymah5',
+    password: 'mSNRRbvOz84R4UbnwF4D'
 });
+
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     database: 'blog',
+//     user: 'root',
+//     port: 3308,
+//     password: ''
+// });
 module.exports = pool;
